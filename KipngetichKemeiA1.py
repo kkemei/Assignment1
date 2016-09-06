e7517eca60d0b5b7a79b59a853044a10b1a91365
kkemei <kipngetich.kemei@my.jcu.edu.au>
2016-09-06 14:09:54 +1000

    got the completeditem to show

--- KipngetichKemeiA1.py
+++ KipngetichKemeiA1.py
@@ -7,6 +7,7 @@ def main():
     items_list = list(csv_file)
     num_of_items = sum(1 for row in items_list)  # calculates how many different items there are
     shopping_list = []
+    number_to_be_marked = []
 
     print("Shopping List 1.0 - by Kipngetich Kemei")
     print("{} items loaded from {}".format(num_of_items, items.name))
@@ -17,13 +18,13 @@ def main():
         if menu_choice == "R":
             required_items = run_required(num_of_items, items_list)
             menu_choice = menu()  # calls the main menu function
-        elif menu_choice == "C":
-            completed_items = run_complete(shopping_list, items_list)
-            menu_choice = menu()  # calls the main menu function
         # elif menu_choice == "A":
         #     add_items = run_add_new()
         elif menu_choice == "M":
-            mark_items = run_mark_item(num_of_items, items_list, shopping_list)
+            mark_items = run_mark_item(num_of_items, items_list, shopping_list, number_to_be_marked)
+            menu_choice = menu()  # calls the main menu function
+        elif menu_choice == "C":
+            completed_items = run_complete(shopping_list, items_list, number_to_be_marked)
             menu_choice = menu()  # calls the main menu function
 
 
@@ -59,7 +60,7 @@ def run_required(num_of_items, items_list):
     return expected_price
 
 
-def run_mark_item(num_of_items, items_list, shopping_list):
+def run_mark_item(num_of_items, items_list, shopping_list, number_to_be_marked):
     number = 0
     for row in items_list:
         # items_list[0][2].sort()
@@ -68,26 +69,29 @@ def run_mark_item(num_of_items, items_list, shopping_list):
 
     expected_price = float(items_list[0][1]) + float(items_list[1][1]) + float(items_list[2][1])
     print("total expected price for {} items is: ${}".format(num_of_items, expected_price))
-    numbers = [0, 1, 2, 3]
-    number_to_be_marked = int(input("Enter the number of of an item to mark as completed"))
-    if number_to_be_marked not in numbers:
+    numbers = [0, 1, 2]
+    number_entered = int(input("Enter the number of of an item to mark as completed"))
+    while number_entered not in numbers:
         print("Invalid option")
-    shopping_list.append(items_list[number_to_be_marked][0])
+        number_entered = int(input("Enter the number of of an item to mark as completed"))
+    number_to_be_marked.append(number_entered)
+    shopping_list.append(items_list[number_entered][0])
     print("{} is marked as complete\n".format(shopping_list[0]))
-    return shopping_list
+    return shopping_list, number_to_be_marked
 
 
-def run_complete(shopping_list, items_list):
+def run_complete(shopping_list, items_list, number_to_be_marked):
+    print(number_to_be_marked)
+    counter = 0
     if not shopping_list:
-        print("No complete items\n")
+        print("\nNo complete items\n")
     else:
-        if shopping_list in items_list:
-            # price = float(shopping_list[0][1])
-            print("test test test")
-            # print("total expected price for {} items is: ${}".format(len(shopping_list), price))
+        for row in shopping_list:
+            print("{}. {:25} $ {} ({})".format(counter, shopping_list[0], items_list[number_to_be_marked[counter]][1], items_list[number_to_be_marked[counter]][2]))
+            # print("{}. {}".format(counter, shopping_list))
+            counter += 1
 
             print(shopping_list)
             print("Completed Items\n")
-
 
 main()
