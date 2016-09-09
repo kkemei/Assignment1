7fdf72e4f2b4e30a0c1e40d74c94c7026f142e70
kkemei <kipngetich.kemei@my.jcu.edu.au>
2016-09-09 12:16:23 +1000

    More or less done. All error checking has been done. Just have to check for minor errors.

--- KipngetichKemeiA1.py
+++ KipngetichKemeiA1.py
@@ -1,16 +1,28 @@
+"""
+CP1404 Assignment 1 - Shopping List
+
+Kipngetich Kemei
+9/9/16
+This program allows the user to see a predetermined shopping list , mark items to buy, see what items have been marked
+as complete and it also allows the user to add a item to the shopping list.
+
+Github Repository: https://github.com/kkemei/Assignment1
+
+"""
+
 import csv
 import operator
 
-def main():
-    items = open('items.csv')
-    csv_file = csv.reader(items)
-    items_list = list(csv_file)
+
+def main():  # The main functions that runs the program
+    items_list = load_items()
+
     num_of_items = calculate_numbers(items_list)
     shopping_list = []
     number_to_be_marked = []
 
     print("Shopping List 1.0 - by Kipngetich Kemei")
-    print("{} items loaded from {}".format(num_of_items, items.name))
+    print("{} items loaded from {}".format(num_of_items, "items.csv"))
 
     menu_choice = menu()  # calls the main menu function
 
@@ -31,32 +43,51 @@ def main():
             num_of_items = calculate_numbers(items_list)
             add_items = run_add_new(items_list)
             menu_choice = menu()  # calls the main menu function
+    num_of_items = calculate_numbers(items_list)
+    quit = run_quit(items_list, num_of_items)
 
 
-def calculate_numbers(items_list):
-    num_of_items = sum(1 for row in items_list)  # calculates how many different items there are
+"""
+function load_items:
+    items = open(csv file)
+    csv_file = csv.reader(items)
+    items_list = list(csv_file)
+
+    return items_list
+"""
+
+
+def load_items():  # The function that loads in the csv file and passes it back to the main
+    items = open('items.csv')
+    csv_file = csv.reader(items)
+    items_list = list(csv_file)
+    return items_list
+
+
+def calculate_numbers(items_list):  # This function calculates how many items there are currently in the program
+    num_of_items = sum(1 for row in items_list)
     return num_of_items
 
 
-def menu():
+def menu():  # this function is the main menu. Allows the user to access different functions
     menu_options = ['R', 'C', 'A', 'M', 'Q']
 
     menu_choice = input(
         "Menu: \n R - List required items \n C - List completed items \n A - Add new item \n M - Mark an item as "
-        "completed \n Q - Quit")
+        "completed \n Q - Quit\n >>>")
     menu_choice = menu_choice.upper()
 
     while menu_choice not in menu_options:  # Error checking for values entered in menu
         print("Invalid option")
         menu_choice = input(
             "Menu: \n R - List required items \n C - List completed items \n A - Add new item \n M - Mark an item as "
-            "completed \n Q - Quit\n")
+            "completed \n Q - Quit\n >>>")
         menu_choice = menu_choice.upper()
 
     return menu_choice
 
 
-def run_required(num_of_items, items_list):
+def run_required(num_of_items, items_list):  # This function displays all the items inside the items list
     counter = 0
     expected_price_list = []
     print("Required items:")
@@ -71,30 +102,67 @@ def run_required(num_of_items, items_list):
     return expected_price
 
 
-def run_mark_item(num_of_items, items_list, shopping_list, number_to_be_marked):
-    number = 0
+"""
+function run_complete(shopping_list, items_list, number_to_be_marked):
+    items_counter = 0
+    expected_price_list = []
+
+    for each row in items_list:
+        print all the items' name, price and priority
+        append price to expected_price_list
+        items_counter += 1
+    sum all the values in expected_prices_list
+
+    display total price expected price of all the items just shown
+    get number_entered
+    while true
+        try:
+            get number_entered
+            if number_entered not in range(0, num_of_items) or number_entered in number_to_be_marked:
+                display Invalid option
+            else:
+                break
+        except ValueError:
+            display Invalid input; enter a valid number
+    append the number entered to the number to be marked list
+    append the item marked to the shopping list
+    change the item_required to completed
+    display the items that has been marked as completed
+    return shopping_list and number_to_be_marked
+"""
+
+
+def run_mark_item(num_of_items, items_list, shopping_list,
+                  number_to_be_marked):  # Allows the user to mark a item as complete
+    items_counter = 0
     expected_price_list = []
     items_list.sort(key=operator.itemgetter(2))
     for row in items_list:
-        print("{}. {:25} $ {:7} ({})".format(number, row[0], row[1], row[2]))
-        expected_price_list.append(float((items_list[number][1])))
-        number += 1
+        print("{}. {:25} $ {:7} ({})".format(items_counter, row[0], row[1], row[2]))
+        expected_price_list.append(float((items_list[items_counter][1])))
+        items_counter += 1
     expected_price = sum(expected_price_list)
 
     print("total expected price for {} items is: ${}".format(num_of_items, expected_price))
-    number_entered = int(input("Enter the number of of an item to mark as completed"))
-    while number_entered not in range(0, num_of_items):
-        print("Invalid option")
-        number_entered = int(input("Enter the number of of an item to mark as completed"))
+    while True:
+        try:
+            number_entered = int(input("Enter the number of of an item to mark as completed\n >>>"))
+            if number_entered not in range(0, num_of_items) or number_entered in number_to_be_marked:
+                print("Invalid option")
+            else:
+                break
+        except ValueError:
+            print("Invalid input; enter a valid number")
+
     number_to_be_marked.append(number_entered)
     shopping_list.append(items_list[number_entered][0])
     items_list[number_entered][3] = "c"
-    print(items_list)
     print("{} is marked as complete\n".format(shopping_list[-1]))
     return shopping_list, number_to_be_marked
 
 
-def run_complete(shopping_list, items_list, number_to_be_marked):
+def run_complete(shopping_list, items_list,
+                 number_to_be_marked):  # shows all the items that have been marked as complete
     counter = 0
     expected_price_list = []
     num_of_items = sum(1 for row in shopping_list)  # calculates how many different items there are
@@ -116,35 +184,41 @@ def run_complete(shopping_list, items_list, number_to_be_marked):
     return shopping_list
 
 
-def run_add_new(items_list):
+def run_add_new(items_list):  # allows the user to add a item to the programs item list
     priority_list = ["1", "2", "3"]
-    item_name = str(input("Please enter a item name:"))
+    item_name = str(input("Please enter a item name:\n >>>"))
     while not item_name:
         print("Input can not be blank")
-        item_name = input("Please enter a item name:")
-    print("successful")
+        item_name = input("Please enter a item name:\n >>>")
 
-    item_price = int(input("Item Price:"))
-    while not item_price:
+    while True:
         try:
-            print("Invalid input; enter a valid number")
-            item_price = int(input("Item Price:"))
+            item_price = float(input("Item Price:\n >>>"))
+            if item_price < 0:
+                print("Invalid input; Price must be greater that 0")
+            else:
+                break
         except ValueError:
             print("Invalid input; enter a valid number")
-            item_price = int(input("Item Price:"))
-    print("successful")
 
-    item_priority = input("Please enter the items priority:")
+    item_priority = input("Please enter the items priority:\n >>>")
     while item_priority not in priority_list:
         print("Invalid input; Priority must be either 1, 2 or 3")
-        item_priority = input("Please enter the items priority:")
+        item_priority = input("Please enter the items priority:\n >>>")
 
     item_required = "r"
 
     items_list.append([item_name, item_price, item_priority, item_required])
-    print(items_list)
-    print("{}, ${}, priority ({}) added to shopping list".format(item_name, item_price, item_priority))
+    print("{}, ${}, priority ({}) added to shopping list".format(item_name, float(item_price), item_priority))
     return items_list
+
+
+def run_quit(items_list, num_of_items):  # runs when the user quits and prints all the items to the csv
+    with open('items.csv', "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerows(items_list)
+    print("{} items saved to items.csv \n"
+          "Have a nice day :)".format(num_of_items))
 
 
 main()
